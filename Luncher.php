6d7bf790a485b665ef5bab98a7fb927e20bc057d
hamed95 <h.mn1368@gmail.com>
2016-08-07 00:01:23 -0700

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '**176977435:AAGvb1_CaK8zxm6OmklqcXfITR8srEqA-IQ**');
+define('BOT_TOKEN', '176977435:AAGvb1_CaK8zxm6OmklqcXfITR8srEqA-IQ');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
